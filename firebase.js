31e4b290a4c4a2cbd55800a0d2fa27b312b2aa87
Rahul-2822 <rahul15sheoran@gmail.com>
2024-10-19 16:57:51 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYn78-SO7nzVtOfcIAPDn-WfC7Qq3wUAs",
+  authDomain: "rahul-6e8c5.firebaseapp.com",
+  projectId: "rahul-6e8c5",
+  storageBucket: "rahul-6e8c5.appspot.com",
+  messagingSenderId: "77536140017",
+  appId: "1:77536140017:web:c47f5915023b01f9b3e750",
+  measurementId: "G-EC116GLGSB"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore and Storage
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export { firestore, storage };
